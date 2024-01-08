32b0f4a498bd9ff5263bcd790877eb0a738a590b
Vaishnavi Thakre <86373178+VaishnaviThakre@users.noreply.github.com>
2024-01-08 19:33:31 +0530

    Add files via upload

---
+++ StarProgram.java
@@ -0,0 +1,20 @@
+
+public class StarProgram {
+
+    
+    public static void main(String[] args) {
+        int row=6,i,j;
+        for(i=0;i<row;i++)
+        {
+            for(j = 0;j<=i;j++)
+            {
+                System.out.print("*");
+                
+            }
+            System.out.println();
+        }
+        
+       
+    }
+    
+}
---
+++ StarProgram2.java
@@ -0,0 +1,22 @@
+
+public class StarProgram2 {
+
+   
+    public static void main(String[] args) {
+        
+        int row=5,i,j;
+        for(i=row;i>=1;i--)
+        {
+            
+            for(j=0;j<=i;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+           
+    
+        }
+        
+    }
+    
+}

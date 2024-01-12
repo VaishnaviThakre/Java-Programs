ee12950f7ec32e7cce911dbf5f1ba5bfb20897a0
Vaishnavi Thakre <86373178+VaishnaviThakre@users.noreply.github.com>
2024-01-12 14:38:10 +0530

    Add files via upload

---
+++ ArrayMaxMin.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class ArrayMaxMin {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number of elements in the array:");
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        int max = arr[0];
+        int min = arr[0];
+        for (int i = 1; i < n; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+        }
+        System.out.println("The maximum element in the array is " + max);
+        System.out.println("The minimum element in the array is " + min);
+    }
+}

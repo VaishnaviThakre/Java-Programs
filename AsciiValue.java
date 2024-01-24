572220a0e08cfae5222a3282d21a489beb2e2a9c
Vaishnavi Thakre <86373178+VaishnaviThakre@users.noreply.github.com>
2024-01-24 15:26:12 +0530

    Add files via upload

---
+++ AsciiValue.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class AsciiValue {
+
+
+    public static void main(String[] args) {
+        
+        char ch = 'a';
+        int ascii = ch;
+        
+        System.out.println("The ascii value of a is "+ascii);
+        
+    }
+    
+}

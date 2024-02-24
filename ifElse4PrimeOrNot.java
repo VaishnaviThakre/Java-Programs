7b7094dda0fd37e225cb5bde83e10d3c510ac54f
Vaishnavi Thakre <86373178+VaishnaviThakre@users.noreply.github.com>
2024-02-24 11:52:49 +0530

    Add files via upload

--- ifElse4PrimeOrNot.java
+++ ifElse4PrimeOrNot.java
@@ -1,20 +1,26 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author dell
- */
+import java.util.Scanner;
+ 
 public class ifElse4PrimeOrNot {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = input.nextInt();
+        boolean isPrime = true;
+        if (num == 1 || num == 0) {
+            isPrime = false;
+        } else {
+            for (int i = 2; i <= num / 2; i++) {
+                if (num % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+        if (isPrime) {
+            System.out.println(num + " is a prime number.");
+        } else {
+            System.out.println(num + " is not a prime number.");
+        }
     }
-    
 }
+

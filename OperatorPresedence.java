0e627ec12dcc7c51c47e20bdab6f3d9deac6c2a9
Vaishnavi Thakre <86373178+VaishnaviThakre@users.noreply.github.com>
2024-02-25 20:10:36 +0530

    Add files via upload

--- OperatorPresedence.java
+++ OperatorPresedence.java
@@ -1,20 +1,23 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author dell
- */
 public class OperatorPresedence {
+  public static void main(String[] args) {
+    int a = 5;
+    int b = 10;
+    int c = 15;
+    int d = 20;
+    int e = 25;
+    int f = 30;
+    int g = 35;
+    int h = 40;
+    int i = 45;
+    int j = 50;
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
+    int result1 = a + b * c / d % e;
+    int result2 = (a + b) * (c - d) % e;
+    int result3 = ++f - --g * h++ / ++i % --j;
+
+    System.out.println("The result of " + a + " + " + b + " * " + c + " / " + d + " % " + e + " is " + result1);
+    System.out.println("The result of (" + a + " + " + b + ") * (" + c + " - " + d + ") % " + e + " is " + result2);
+    System.out.println("The result of ++" + f + " - --" + g + " * " + h + "++ / ++" + i + " % --" + j + " is " + result3);
+  }
 }
+

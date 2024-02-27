c7f2f328ae34aa4aa529bc1d0178471a09a9df60
Vaishnavi Thakre <86373178+VaishnaviThakre@users.noreply.github.com>
2024-02-27 16:32:01 +0530

    Add files via upload

--- static4Exponent.java
+++ static4Exponent.java
@@ -1,20 +1,26 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+import java.util.Scanner;
 
-/**
- *
- * @author dell
- */
 public class static4Exponent {
+    static class Calculator {
+        static double PI = 3.14159265358979323846;
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+        static int power(int base, int exponent) {
+            int result = 1;
+            for (int i = 0; i < exponent; i++) {
+                result *= base;
+            }
+            return result;
+        }
     }
-    
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the base number: ");
+        int base = scanner.nextInt();
+        System.out.print("Enter the exponent: ");
+        int exponent = scanner.nextInt();
+        int result = Calculator.power(base, exponent);
+        System.out.printf("%d raised to the power of %d is %d", base, exponent, result);
+    }
 }
+
